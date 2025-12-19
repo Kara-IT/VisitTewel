0e972aff1c04eb3710dcb1d10e9840fa2c430f78
Ngakan Made Alit Wiradhanta <alitwiradhanta094@student.unud.ac.id>
2025-12-19 16:58:26 +0800

    Update Marque.jsx

--- app/Components/Marque.jsx
+++ app/Components/Marque.jsx
@@ -5,8 +5,8 @@ export default function Marque() {
     <div className="bg-primary z-10 fixed top-18 h-8 w-full md:block hidden">
       <div className="flex items-center justify-center h-full">
         <h1 className="text-xs text-white ">
-          Jl. Raya Ketewel, Kecamatan Sukawati, Kabupaten Gianyar. Email:
-          desaketewel04@gmail.com. Telp: (0361)297474.
+          Jln.Pura Margatuga, Br. Puseh, Ketewel, Sukawati, Gianyar - Bali. Email:
+          desaadatketewel01@gmail.com. Telp: +6285792558149. FB:DesaAdat Ketewel. IG:Visit_Ketewel. Tiktok:@visit_ketewel.
         </h1>
       </div>
     </div>
